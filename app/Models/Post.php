491df4fe8d4d82a73711fa21ccd7958bafa30fa6
NecipAkgz <nexco@hotmail.com>
2024-02-23 03:37:09 +0300

    implement search functionalty

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,6 +14,17 @@ class Post extends Model
     protected $guarded = [];
 
     protected $with = ['category', 'author'];
+
+    public function scopeFilter(Builder $query, array $filters)
+    {
+        $query
+            ->when($filters['search'] ?? false,
+                fn ($query, $search) => $query
+                    ->where('title', 'like', '%'.$search.'%')
+                    ->orWhere('body', 'like', '%'.$search.'%')
+            );
+
+    }
 
     public function category()
     {
