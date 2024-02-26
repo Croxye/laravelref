c7f368ba7f22f64bd64dfbc97e4fef9b21eba2d9
NecipAkgz <nexco@hotmail.com>
2024-02-27 02:10:23 +0300

    feat: changed newsletter approach to interface approach

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsletter;
+use App\Services\Newsletter;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Bind Newsletter interface to a callback function
+        app()->bind(Newsletter::class, function () {
+            // Create a new instance of ApiClient
+            $client = new ApiClient();
+
+            // Set configuration for the client
+            $client->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us14',
+            ]);
+
+            // Return a new MailchimpNewsletter instance with the configured ApiClient
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
---
+++ app/Services/MailchimpNewsletter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use MailchimpMarketing\ApiClient;
+
+class MailchimpNewsletter implements Newsletter
+{
+    public function __construct(protected ApiClient $client)
+    {
+    }
+
+    public function subscribe(string $email, ?string $listId = null)
+    {
+        $listId = $listId ?? config('services.mailchimp.lists.subscribers');
+
+        return $this->client->lists->addListMember($listId, [
+            'email_address' => $email,
+            'status' => 'subscribed',
+        ]);
+    }
+}
